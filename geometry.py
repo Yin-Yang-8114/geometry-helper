476bf688d08f3627be01c9c501d6c0835e3b50a1
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-25 10:00:49 +0300

    square area added

---
+++ geometry.py
@@ -0,0 +1,5 @@
+# square area calculater
+given_square_side=int(input("please enter your choosen square side: "))
+print("the square area is",given_square_side * given_square_side)
+# circle area calculator
+given_cirdle_radius=int(input("please enter your "))
