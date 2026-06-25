6dbf6aec4509b9bc8c8ccbe2db734a849ab0fb28
gadb633-cpu <gadb633@gmail.com>
2026-06-25 12:25:05 +0300

    Implement Pythagorean triad check for user input
    Added functionality to check if three numbers form a Pythagorean triad.

--- geometry.py
+++ geometry.py
@@ -37,8 +37,32 @@ while exit1:
         given_circle_radius=float(input("please enter your circle radius: "))
         print("the circle erea is", given_circle_radius * 2 * 3.141592654)
 
-    elif user_choice == "5":
-        pass
+    elif user_choice == "5": 
+        numbers = input("giva me three num: ")
+        list_numbers = numbers.split()
+
+
+        list_float = []
+        for list_num in list_numbers:
+        list_float.append(float(list_num))
+        num0 = list_float[0]
+        num1 = list_float[1]
+        num2 = list_float[2]
+        if num0 > num1 and num0 > num2:
+            if num0 ** 2 == num1 ** 2 + num2 ** 2:
+                print(num0,num1,num2, "are a pythagorean triad ")
+            else:
+            print("are not a pythagorean triad")    
+        elif num1 > num0 and num1 > num2:
+            if num1 ** 2 == num0 ** 2 + num2 ** 2:
+                print(num0,num1,num2, "are a pythagorean triad ")
+            else:
+                print("are not a pythagorean triad")    
+        elif num2 > num0 and num2 > num1:
+            if num2 ** 2 == num0 ** 2 + num1 ** 2:
+                print(num0,num1,num2, "are a pythagorean triad ")
+            else:
+                print("are not a pythagorean triad") 
     elif user_choice == "6":
         print("goodbye")
         exit1 = False
