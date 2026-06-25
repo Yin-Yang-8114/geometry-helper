410521cf8558bdb177a7e65896f1fd86f3059c85
Yin Yang <mihiparis@gmail.com>
2026-06-25 10:05:47 +0300

    created while loop and the start menu

---
+++ geometry.py
@@ -0,0 +1,16 @@
+exit1 = True
+while exit1:
+    user_choice = input(f'welcome to geometry helper \n please enter your choice: \n 1) calculator  : \n 2) Triangle area calculator:  \n 3) Square area calculator:  \n 4) Circle area calculator:  \n 5) Pythagoras triad checker : \n 6) Exit : \n ')
+    if user_choice == "1":
+        pass
+    elif user_choice == "2":
+        pass
+    elif user_choice == "3":
+        pass
+    elif user_choice == "4":
+        pass
+    elif user_choice == "5":
+        pass
+    elif user_choice == "6":
+        print("goodbye")
+        exit1 = False
