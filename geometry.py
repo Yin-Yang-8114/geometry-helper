a31075dcbc5f1679d320fa1de7bdd3a9a5b2c348
gad-biton <gadb633@gmail.com>
2026-06-25 11:56:04 +0300

    added triangle area

--- geometry.py
+++ geometry.py
@@ -6,27 +6,6 @@ print("the triangle area is:",(calculation))
 
 
 
-# pythagoras 
-numbers = input("giva me three num (for split enter . )")
-list_numbers = numbers.split(".")
-print(list_numbers)
-
-list_float = []
-for list_num in list_numbers:
-    list_float.append(float(list_num))
-print(list_float)
-
-max_num = max(list_float)
-print(max_num)
-
-min_num = min(list_float)
-print(min_num)
-
-middel_num = list_float[0]
-print(middel_num)
-
-print(list_float)
-# if max_num ** 2 == 
 
 
     
