b4439dd4363db07e9a76aaddc296a582bd2c606f
gad-biton <gadb633@gmail.com>
2026-06-25 10:00:20 +0300

    added triangle area calculator

---
+++ geometry.py
@@ -0,0 +1,7 @@
+# triangle area calculator
+num_height = int(input("give me num for height "))
+num_base = int(input("give me num for base "))
+calculation = num_base * num_height / 2
+print("the triangle area is:",(calculation))
+
+
