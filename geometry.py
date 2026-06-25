997fe819f259e67f487dcd03626fd6dac7e0ee06
gadb633-cpu <gadb633@gmail.com>
2026-06-25 12:30:21 +0300

    Fix indentation for Pythagorean triad check

--- geometry.py
+++ geometry.py
@@ -52,7 +52,7 @@ while exit1:
             if num0 ** 2 == num1 ** 2 + num2 ** 2:
                 print(num0,num1,num2, "are a pythagorean triad ")
             else:
-            print("are not a pythagorean triad")    
+                print("are not a pythagorean triad")    
         elif num1 > num0 and num1 > num2:
             if num1 ** 2 == num0 ** 2 + num2 ** 2:
                 print(num0,num1,num2, "are a pythagorean triad ")
