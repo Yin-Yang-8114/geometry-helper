f4a424e090b88e589d4cf46344552867de4eea14
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-25 10:19:37 +0300

    circle area added

--- geometry.py
+++ geometry.py
@@ -2,4 +2,6 @@
 given_square_side=int(input("please enter your choosen square side: "))
 print("the square area is",given_square_side * given_square_side)
 # circle area calculator
-given_cirdle_radius=int(input("please enter your "))
+given_cirdle_radius=int(input("please enter your circle radius: "))
+print(given_cirdle_radius * 2 * 3.141592654)
+
