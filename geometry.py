ca83323e7a98eb57e0b9a03b98bc71e640bc7fb9
gad-biton <gadb633@gmail.com>
2026-06-25 11:27:33 +0300

    added triangle area calculator 2

--- geometry.py
+++ geometry.py
@@ -1,7 +1,33 @@
 # triangle area calculator
-num_height = int(input("give me num for height "))
-num_base = int(input("give me num for base "))
+num_height = float(input("give me num for height "))
+num_base = float(input("give me num for base "))
 calculation = num_base * num_height / 2
 print("the triangle area is:",(calculation))
 
 
+
+# pythagoras 
+numbers = input("giva me three num (for split enter . )")
+list_numbers = numbers.split(".")
+print(list_numbers)
+
+list_float = []
+for list_num in list_numbers:
+    list_float.append(float(list_num))
+print(list_float)
+
+max_num = max(list_float)
+print(max_num)
+
+min_num = min(list_float)
+print(min_num)
+
+middel_num = list_float[0]
+print(middel_num)
+
+print(list_float)
+# if max_num ** 2 == 
+
+
+    
+ 
