4a7e5273ca1bad1e923bae8b2fe6719fcc33bfff
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-25 10:24:50 +0300

    flout added

--- geometry.py
+++ geometry.py
@@ -1,7 +1,7 @@
 # square area calculater
-given_square_side=int(input("please enter your choosen square side: "))
+given_square_side=float(input("please enter your choosen square side: "))
 print("the square area is",given_square_side * given_square_side)
 # circle area calculator
-given_cirdle_radius=int(input("please enter your circle radius: "))
-print(given_cirdle_radius * 2 * 3.141592654)
-
+given_circle_radius=float(input("please enter your circle radius: "))
+print("the circle erea is", given_circle_radius * 2 * 3.141592654)
+# 
